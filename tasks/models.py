1ac3a640680884030d7f6f00ebfd57d7dcd20e64
Madaraka02 <victormadaraka@gmail.com>
2023-06-21 01:43:20 +0300

    added task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    TASK_STATUS_CHOICES = (
+    ("new", "new"),
+    ("progress", "progress"),
+    ("complete", "complete"),
+    )
+    description = models.TextField(null=True, blank=True)
+    end_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(null=True, default='new', choices=TASK_STATUS_CHOICES, max_length=20)
+    over_due = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Task {self.id}'
+    
+    
