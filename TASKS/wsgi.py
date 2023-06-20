cc35779ad7e194613ed1923204ad302216ea4476
Madaraka02 <victormadaraka@gmail.com>
2023-06-21 01:27:02 +0300

    git ignore

---
+++ TASKS/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TASKS project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TASKS.settings')
+
+application = get_wsgi_application()
